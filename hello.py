f9f306308ab9c3395643520a08f897f7277ef71a
ammahuifang <yhfyanghuifang@126.com>
2017-12-07 15:14:08 +0800

    This is new

---
+++ hello.py
@@ -0,0 +1,3 @@
+print('Hello Python!')
+print('Can I make friends with you?')
+print('I need your help!')
---
+++ test.py
@@ -0,0 +1,7 @@
+print('1+1=?')
+print(1+1)
+
+print('hello world!')
+print('hello python!')
+
+print(list(range(5)))
